2fbed559a78400b7bcb30a17f0ca7c317d9f4012
Minh Khiêm <phamminhkhiem16@gmail.com>
2024-10-16 17:22:10 +0700

    link chat/cart page to navbar

--- ticket-resell/src/pages/ShoppingCart/ShoppingCart.tsx
+++ ticket-resell/src/pages/ShoppingCart/ShoppingCart.tsx
@@ -2,25 +2,33 @@ import React from "react";
 import "./ShoppingCart.css";
 import TicketContent from "../../components/ShoppingCartComponent/TicketContent/TicketContent";
 import SummaryCost from "../../components/ShoppingCartComponent/SummaryCost/SummaryCost";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
+import NavBar from "../../components/NavBar";
+
 const ShoppingCart = () => {
   return (
     <div className="cart">
+      <div className="navbar">
+        <NavBar />
+      </div>
       <div className="cart-wrapper">
-          <h2 className="section-title">Shopping Cart</h2>
-        <motion.div className="cart-container"
-        initial={{ opacity:0, y: 100 }}
-        animate={{ opacity:1, y: 0 }}
-        transition={{ duration: 2 }}
+        <h2 className="section-title">Shopping Cart</h2>
+        <motion.div
+          className="cart-container"
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 2 }}
         >
           <div className="cart-items">
+            <TicketContent />
+            <TicketContent />
             <TicketContent />
             <TicketContent />
             <TicketContent />
           </div>
 
           <div className="summary">
-          <SummaryCost />
+            <SummaryCost />
           </div>
         </motion.div>
       </div>
