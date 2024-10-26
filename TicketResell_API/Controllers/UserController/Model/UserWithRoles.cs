0c8cac4bc4803067e055b9082294ccac185de556
dotrongnghia2101 <dotrongnghia2101@gmail.com>
2024-10-26 21:38:53 +0700

    update get all user api in admin

---
+++ TicketResell_API/Controllers/UserController/Model/UserWithRoles.cs
@@ -0,0 +1,15 @@
+﻿namespace TicketResell_API.Controllers.UserController.Model
+{
+    public class UserWithRoles
+    {
+        public string userId { get; set; }
+        public string userName { get; set; }
+        public string email { get; set; }
+        public string? firstName { get; set; }
+        public string? lastName { get; set; }
+        public string? bio { get; set; }
+        public string? gender { get; set; }
+        public string? address { get; set; }
+        public IList<string> roles { get; set; }
+    }
+}
